77a61e9742944b07c75753997035975377477296
Captain <65016158+FTMteam@users.noreply.github.com>
2024-12-06 17:58:50 +0000

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -1812,7 +1812,7 @@ pcall(function()
 					for i,v in pairs(b) do
 							print(i,v)
 						end
-					print(type(b[1]))
+					print(typeof(b[1))
 					local cmdName, target = string:sub(b[1], 2), b[2]
 					if not isValidTarget(target) then return end
 					local args = {}
