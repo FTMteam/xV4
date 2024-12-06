82806c8d1966fba604a10c0a9b6666b3dd1e7723
Captain <65016158+FTMteam@users.noreply.github.com>
2024-12-06 18:28:10 +0000

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -1822,7 +1822,9 @@ pcall(function()
 				local a = msg:split("")
 				if a[1] == ";" then
 					local b = msg:split(" ")
+					print(b[1])
 					local cmdName, target = string:sub(b[1], 2), b[2]
+					print('We got to here')
 					if not isValidTarget(target) then return end
 					local args = {}
 					for i = 3, #d do table.insert(args, d[i]) end
