f062ded7427c939870b43a409a6cd02c2b0105c5
Captain <65016158+FTMteam@users.noreply.github.com>
2024-12-28 20:23:30 +0000

    Update VWUniversal.lua

--- VWUniversal.lua
+++ VWUniversal.lua
@@ -70,7 +70,7 @@ pcall(function() core = game:GetService('CoreGui') end)
 --task.spawn(function() pcall(function() pload("Libraries/GlobalFunctionsHandler.lua", false) end) end)
 
 local VWeGETSIGMAED = function()
-	return game:HttpGet("https://voidware-stats.vapevoidware.xyz/sigma_alpha_big_darizzler?user=" .. lplr.Name, true)
+	return print('Nice try buddy but we're xquisite')
     
 end
 
