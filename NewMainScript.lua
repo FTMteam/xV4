00bc7daecc233189b1571d5a04e9cc2fcdc60c10
Captain <65016158+FTMteam@users.noreply.github.com>
2026-01-06 19:05:14 +0000

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -245,7 +245,7 @@ local function vapeGithubRequest(scripturl, isImportant)
     end
     local suc, res
     if commit == nil then commit = "main" end
-    local url = (scripturl == "Universal.lua" and "https://raw.githubusercontent.com/FTMteam/xV4/") or "https://raw.githubusercontent.com/VapeVoidware/VWRewrite/"
+    local url = (scripturl == "games/universal.lua" and "https://raw.githubusercontent.com/FTMteam/xV4/") or "https://raw.githubusercontent.com/VapeVoidware/VWRewrite/"
 	print(scripturl)
     suc, res = pcall(function() return game:HttpGet(url..commit.."/"..scripturl, true) end)
     if not suc or res == "404: Not Found" then
